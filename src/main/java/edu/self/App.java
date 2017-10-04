6f28379b4ae8978b15215e27597c27bbdf9829fe
shino <masataka_shinohara@r.recruit.co.jp>
2017-10-04 12:51:58 +0900

    [Update] JSONを返す [Update] 静的ファイル配信

--- src/main/java/edu/self/App.java
+++ src/main/java/edu/self/App.java
@@ -1,22 +1,30 @@
-package my_first_spring_boot;
+package edu.self;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController // Webアプリのリクエストを受け付けるクラスであることの指定
 @EnableAutoConfiguration // 様々な設定を自動的に有効化
 public class App {
-  @RequestMapping("/") // URLのパスを指定
+  @RequestMapping("/")
   public String index() {
     return "Hello Spring Boot!";
   }
 
-  public static void main( String[] args )
-    {
-      // Spring Bootによるアプリケーションを起動する
-      SpringApplication.run(App.class, args);
-    }
+  // JSONを返す
+  @RequestMapping("/hello")
+  public Map<String, String> hello() {
+    return Collections.singletonMap("message", "Hello, World!");
+
+  }
+
+  public static void main( String[] args ) {
+    // Spring Bootによるアプリケーションを起動する
+    SpringApplication.run(App.class, args);
+  }
 }
